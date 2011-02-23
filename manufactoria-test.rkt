f7d67c73a2baf7aa5ebcf25e79d22564e3a30438
Francisco Dibar <frandibar@gmail.com>
2011-02-22 21:28:54 -0300

    initial commit

---
+++ manufactoria-test.rkt
@@ -0,0 +1,185 @@
+#! /usr/local/bin/racket
+#lang racket
+
+(require rackunit)
+(require rackunit/text-ui)
+(require (only-in "manufactoria.rkt"
+                  build-machine
+                  run
+                  ))
+
+(define manufactoria-test-suite 
+  (test-suite 
+   "Tests for manufactoria.rkt"
+
+   (test-case
+    "Robotoast! l1 - Accept everything"
+     (let ([machine (build-machine "?lvl=1&code=c12:6f3;c12:7f3;c12:8f3;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted (b)) "Test 2")
+       (check-equal? (run machine '(r)) '(accepted (r)) "Test 3")
+       (check-equal? (run machine '(b r)) '(accepted (b r)) "Test 4")
+       ))
+
+   (test-case
+    "Robocoffee! l2 - Accept if starts with blue"
+     (let ([machine (build-machine "?lvl=2&code=p12:6f3;c11:6f3;c11:7f3;c11:8f3;c11:9f2;")])
+       (check-equal? (run machine '()) '(rejected ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted ()) "Test 2")
+       (check-equal? (run machine '(b r)) '(accepted (r)) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted (b)) "Test 4")
+       (check-equal? (run machine '(r b)) '(rejected (b)) "Test 5")
+       ))
+
+   (test-case
+    "Robolamp! l3 - Accept if three or more blues"
+     (let ([machine (build-machine "?lvl=3&code=c12:5f3;p12:6f3;c13:5f0;c13:6f1;c11:6f0;p10:7f3;c9:7f3;c10:6f3;c11:7f1;c9:9f2;c10:9f2;c9:8f3;c11:8f3;c11:9f2;c12:8f0;p12:9f2")])
+       (check-equal? (run machine '()) '(rejected ()) "Test 1")
+       (check-equal? (run machine '(b)) '(rejected ()) "Test 2")
+       (check-equal? (run machine '(b b)) '(rejected ()) "Test 3")
+       (check-equal? (run machine '(b b b)) '(accepted ()) "Test 4")
+       (check-equal? (run machine '(r b b b)) '(accepted ()) "Test 5")
+       (check-equal? (run machine '(r b b b r b)) '(accepted (r b)) "Test 6")
+       (check-equal? (run machine '(r b b r r)) '(rejected ()) "Test 7")
+       ))
+
+   (test-case
+    "Robofish! l4 - Accept if no red"
+     (let ([machine (build-machine "?lvl=4&code=p12:8f3;c12:6f3;c12:7f3;c11:8f1;c11:7f2;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted ()) "Test 2")
+       (check-equal? (run machine '(b r)) '(rejected ()) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted ()) "Test 4")
+       (check-equal? (run machine '(r b)) '(rejected (b)) "Test 5")
+       ))
+
+   (test-case
+    "Robobugs! l5 - Accept alternating colors"
+     (let ([machine (build-machine "?lvl=5&code=c12:4f3;c12:8f3;c12:9f3;c12:10f3;p12:6f3;c12:7f3;c12:5f3;c11:6f0;p10:6f0;p10:7f0;c9:6f3;c9:7f3;c9:8f3;c9:9f2;c10:9f2;c11:9f2;c13:6f2;p14:6f2;p14:7f2;c15:6f3;c15:7f3;c15:8f3;c15:9f0;c14:9f0;c13:9f0;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted ()) "Test 2")
+       (check-equal? (run machine '(b r)) '(accepted ()) "Test 3")
+       (check-equal? (run machine '(b b)) '(rejected ()) "Test 4")
+       (check-equal? (run machine '(r r)) '(rejected ()) "Test 5")
+       (check-equal? (run machine '(r b)) '(accepted ()) "Test 6")
+       (check-equal? (run machine '(r b r)) '(accepted ()) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(accepted ()) "Test 8")
+       ))
+
+   (test-case
+    "Robocats! l6 - Accept if ends with 2 blues"
+     (let ([machine (build-machine "?lvl=6&code=c10:10f3;c10:11f2;c11:11f2;c11:5f3;c12:4f3;p12:5f3;c13:4f0;c13:5f1;c13:6f1;c11:6f3;p11:7f3;c12:7f2;c9:7f2;c13:7f1;c9:9f1;p10:9f3;c9:8f1;c10:8f3;c11:9f2;c12:9f2;c13:8f1;c13:9f1;c10:7f3;")])
+       (check-equal? (run machine '()) '(rejected ()) "Test 1")
+       (check-equal? (run machine '(b)) '(rejected ()) "Test 2")
+       (check-equal? (run machine '(b r)) '(rejected ()) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted ()) "Test 4")
+       (check-equal? (run machine '(r r)) '(rejected ()) "Test 5")
+       (check-equal? (run machine '(r b)) '(rejected ()) "Test 6")
+       (check-equal? (run machine '(r b r)) '(rejected ()) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(rejected ()) "Test 8")
+       (check-equal? (run machine '(b r b b)) '(accepted ()) "Test 9")
+       ))
+
+
+   (test-case
+    "Robobears! l7 - Accept if starts and ends with same color"
+     (let ([machine (build-machine "?lvl=7&code=p12:4f3;c12:10f3;c11:4f0;c13:4f2;c12:5f3;c12:6f3;c12:7f3;c12:8f3;c12:9f3;c10:4f0;c8:4f3;c8:5f3;c9:4f0;c11:7f1;c11:5f1;c16:4f3;c16:5f3;c14:4f2;c15:4f2;c13:7f1;c13:5f1;c8:6f3;c8:7f3;c8:8f2;c9:6f3;c9:7f2;c9:8f2;c9:9f1;p10:6f1;c10:7f1;p10:8f2;c10:9f0;c11:6f1;c11:8f2;c13:6f1;c13:8f0;p14:6f1;c14:7f1;p14:8f0;c14:9f2;c15:6f3;c15:7f0;c15:8f0;c15:9f1;c16:6f3;c16:7f3;c16:8f0;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted ()) "Test 2")
+       (check-equal? (run machine '(b r)) '(rejected ()) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted ()) "Test 4")
+       (check-equal? (run machine '(r r)) '(accepted ()) "Test 5")
+       (check-equal? (run machine '(r b)) '(rejected ()) "Test 6")
+       (check-equal? (run machine '(r b r)) '(accepted ()) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(rejected ()) "Test 8")
+       (check-equal? (run machine '(b r b b)) '(accepted ()) "Test 9")
+       ))
+
+   (test-case
+    "RC Cars! l8 - The input with the first symbol at the end"
+     (let ([machine (build-machine "?lvl=8&code=c12:8f3;c12:7f3;p12:6f3;c11:7f2;c13:7f0;b11:6f3;r13:6f3;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted (b)) "Test 2")
+       (check-equal? (run machine '(b r)) '(accepted (r b)) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted (b b)) "Test 4")
+       (check-equal? (run machine '(r r)) '(accepted (r r)) "Test 5")
+       (check-equal? (run machine '(r b)) '(accepted (b r)) "Test 6")
+       (check-equal? (run machine '(r b r)) '(accepted (b r r)) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(accepted (r b r b)) "Test 8")
+       (check-equal? (run machine '(b r b b)) '(accepted (r b b b)) "Test 9")
+       ))
+
+   (test-case
+    "Robocars! l9 - Replace blue with green and red with yellow"
+     (let ([machine (build-machine "?lvl=9&code=g11:6f3;p12:6f3;y13:6f3;c12:7f3;c12:8f3;c12:9f3;c10:6f1;c14:6f1;c14:5f0;c13:5f0;c10:5f2;c11:5f2;c12:5f3;c11:7f0;c10:7f1;c14:7f1;c13:7f2;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted (g)) "Test 2")
+       (check-equal? (run machine '(b r)) '(accepted (g y)) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted (g g)) "Test 4")
+       (check-equal? (run machine '(r r)) '(accepted (y y)) "Test 5")
+       (check-equal? (run machine '(r b)) '(accepted (y g)) "Test 6")
+       (check-equal? (run machine '(r b r)) '(accepted (y g y)) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(accepted (g y g y)) "Test 8")
+       (check-equal? (run machine '(b r b b)) '(accepted (g y g g)) "Test 9")
+       ))
+
+   (test-case
+    "Robostilts! l10 - Put a green at the beginning and a yellow at the end"
+     (let ([machine (build-machine "?lvl=10&code=g12:4f2;p12:6f3;b11:6f1;r13:6f1;c11:5f2;c13:5f0;c12:5f3;y12:7f3;c12:8f3;c12:9f3;c12:10f3;c13:4f3;")])
+       (check-equal? (run machine '()) '(accepted (g y)) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted (g b y)) "Test 2")
+       (check-equal? (run machine '(b r)) '(accepted (g b r y)) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted (g b b y)) "Test 4")
+       (check-equal? (run machine '(r r)) '(accepted (g r r y)) "Test 5")
+       (check-equal? (run machine '(r b)) '(accepted (g r b y)) "Test 6")
+       (check-equal? (run machine '(r b r)) '(accepted (g r b r y)) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(accepted (g b r b r y)) "Test 8")
+       (check-equal? (run machine '(b r b b)) '(accepted (g b r b b y)) "Test 9")
+       ))
+
+   ;; (test-case
+   ;;  "Milidogs! l11 - With blue as 1 and red as 0 accept odd binary strings"
+   ;;   (let ([machine (build-machine "?lvl=11&code=p11:8f2;c12:8f3;c12:9f3;c10:5f3;c10:6f3;c10:7f3;c12:5f0;c11:5f0;c10:9f1;c10:8f2;c11:9f0;c11:7f2;c12:7f2;p13:7f2;c13:8f2;c14:5f0;c13:5f0;c15:6f1;c15:7f1;c15:8f1;c14:8f2;c15:5f0;c13:6f0;c12:6f3;")])
+   ;;     (check-equal? (run machine '()) '(accepted (g y)) "Test 1")
+   ;;     (check-equal? (run machine '(b)) '(accepted (g b y)) "Test 2")
+   ;;     (check-equal? (run machine '(b r)) '(accepted (g b r y)) "Test 3")
+   ;;     (check-equal? (run machine '(b b)) '(accepted (g b b y)) "Test 4")
+   ;;     (check-equal? (run machine '(r r)) '(accepted (g r r y)) "Test 5")
+   ;;     (check-equal? (run machine '(r b)) '(accepted (g r b y)) "Test 6")
+   ;;     (check-equal? (run machine '(r b r)) '(accepted (g r b r y)) "Test 7")
+   ;;     (check-equal? (run machine '(b r b r)) '(accepted (g b r b r y)) "Test 8")
+   ;;     (check-equal? (run machine '(b r b b)) '(accepted (g b r b b y)) "Test 9")
+   ;;     ))
+
+   (test-case
+    "Roborockets! l25 - Swap blue for red and red for blue"
+     (let ([machine (build-machine "?lvl=25&code=c12:5f3;c12:9f3;p12:6f3;g11:6f1;c11:5f2;y13:6f1;c13:5f0;c12:7f3;q12:8f3;c13:7f0;c11:7f2;r13:8f1;b11:8f1;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted (r)) "Test 2")
+       (check-equal? (run machine '(b r)) '(accepted (r b)) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted (r r)) "Test 4")
+       (check-equal? (run machine '(r r)) '(accepted (b b)) "Test 5")
+       (check-equal? (run machine '(r b)) '(accepted (b r)) "Test 6")
+       (check-equal? (run machine '(r b r)) '(accepted (b r b)) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(accepted (r b r b)) "Test 8")
+       (check-equal? (run machine '(b r b b)) '(accepted (r b r r)) "Test 9")
+       ))
+
+   (test-case
+    "Roboplanes! l26 - Remove red"
+     (let ([machine (build-machine "?lvl=26&code=p12:6f3;g11:6f1;c13:5f0;c12:5f3;c13:6f1;c11:5f2;q12:8f3;c12:7f3;b13:8f1;c13:7f0;c12:9f3;")])
+       (check-equal? (run machine '()) '(accepted ()) "Test 1")
+       (check-equal? (run machine '(b)) '(accepted (b)) "Test 2")
+       (check-equal? (run machine '(b r)) '(accepted (b)) "Test 3")
+       (check-equal? (run machine '(b b)) '(accepted (b b)) "Test 4")
+       (check-equal? (run machine '(r r)) '(accepted ()) "Test 5")
+       (check-equal? (run machine '(r b)) '(accepted (b)) "Test 6")
+       (check-equal? (run machine '(r b r)) '(accepted (b)) "Test 7")
+       (check-equal? (run machine '(b r b r)) '(accepted (b b)) "Test 8")
+       (check-equal? (run machine '(b r b b)) '(accepted (b b b)) "Test 9")
+       ))
+   )
+  )
+
+(run-tests manufactoria-test-suite)
+
---
+++ manufactoria.rkt
@@ -0,0 +1,315 @@
+;; manufactoria
+
+;; program that generates the function representing the machine
+
+;; elements
+;; writers (blue, red, green, yellow)
+;; b
+;; r
+;; g
+;; y
+;; c carrier
+;; directions
+;; f0 right to left
+;; f1 bottom to top
+;; f2 left to right
+;; f3 top to bottom
+;; branches
+;; p (blue & red)
+;; q (yellow & green)
+;; directions:
+;; f0 left
+;; f1 up
+;; f2 right
+;; f3 down
+;; f6 inverted right
+;; [element][column]:[row][direction];
+;; in clockwise direction at angle 0
+;; f0 for branches (in yellow none green) (in blue none red)
+;; f1 rotate clockwise (yellow none green in) (blue none red in)
+;; f4 (flip) (in green none yellow) (in red none blue) 
+;; f5 rotate clockwise (green none yellow in) (red none blue in)
+;; f8
+
+;; Check file manufactoria-test.rkt for usage examples
+
+(module 
+ manufactoria racket
+
+ (require (only-in "string-helpers.rkt"
+                   string-null?
+                   string-split))
+ (provide 
+  build-machine
+  run)
+
+ (define *branch-dirs* `((p ((f0 ((b up) (,null left) (r down)))
+                             (f1 ((b right) (,null up) (r left)))
+                             (f2 ((b down) (,null right) (r up)))
+                             (f3 ((b left) (,null down) (r right)))
+                             (f4 ((r up) (,null left) (b down)))
+                             (f5 ((r right) (,null up) (b left)))
+                             (f6 ((r down) (,null right) (b up)))
+                             (f7 ((r left) (,null down) (b right)))))
+                         (q ((f0 ((y up) (,null left) (g down)))
+                             (f1 ((y right) (,null up) (g left)))
+                             (f2 ((y down) (,null right) (g up)))
+                             (f3 ((y left) (,null down) (g right)))
+                             (f4 ((g up) (,null left) (y down)))
+                             (f5 ((g right) (,null up) (y left)))
+                             (f6 ((g down) (,null right) (y up)))
+                             (f7 ((g left) (,null down) (y right)))))))
+
+ ;; The starting position depends on the the level
+ ;; Each entry contains (level (nrows (start-col start-row) (end-col end-row)))
+ (define *levels* '((1 (5 (12 6) (12 9)))
+                    (2 (5 (12 6) (12 9)))
+                    (3 (7 (12 5) (12 10)))
+                    (4 (5 (12 6) (12 9)))
+                    (5 (9 (12 4) (12 11)))
+                    (6 (9 (12 4) (12 11)))
+                    (7 (9 (12 4) (12 11)))
+                    (8 (5 (12 6) (12 9)))
+                    (9 (7 (12 5) (12 10)))
+                    (10 (9 (12 4) (12 11)))
+                    (25 (7 (12 5) (12 10)))
+                    (26 (7 (12 5) (12 10)))
+                    ))
+
+ (define (get-start-pos lvl)
+   (cadadr (assoc lvl *levels*)))
+
+ (define (get-finish-pos lvl)
+   (last (last (assoc lvl *levels*))))
+
+ (define (get-level hardware-desc)
+   (string->number (second (string-split hardware-desc '(#\; #\& #\=)))))
+
+ ;; Returns a list of tokens extracted from the machine's string representation
+ ;; hardware-desc is a string such as "?lvl=1&code=c12:6f3;c12:7f3;c12:8f3;"
+ (define (split-into-tokens hardware-desc)
+   ;; convert "c12:6f3" into ((12 6) c f3)
+   (define (conv-notation str)
+     (if (string-null? str)
+         '()
+         (let* ([toks (string-split str '(#\:))]
+                [fst (car toks)]
+                [sec (second toks)])
+           (let ([type (string->symbol (string (string-ref fst 0)))]
+                 [col (string->number (substring fst 1 (string-length fst)))]
+                 [row (string->number (substring sec 0 (- (string-length sec) 2)))]
+                 [dir (string->symbol (substring sec (- (string-length sec) 2) (string-length sec)))])
+             (list (list col row) type dir)))))
+
+   (let ([toks (cdddr (string-split hardware-desc '(#\; #\& #\=)))])   ; remove first 2 elements
+     ;; add basket token (ending token)
+     (filter (lambda (x) (not (null? x)))
+             (append (map conv-notation toks) (list (basket-tok (get-level hardware-desc)))))))
+
+ ;; Returns the basket token
+ (define (basket-tok level)
+   (list (get-finish-pos level)
+         'basket
+         null))
+
+ ;; Returns a unique name for the token, used for function calls.
+ ;; i.e. token in position (12 3) gets called "fn-12-3"
+ (define (token-id token)
+   (string->symbol (string-join (append '("fn") (map number->string (token-pos token))) "-")))
+
+ ;; Returns the token's position
+ (define (token-pos token)
+   (car token))
+
+ ;; Returns the token's type
+ ;; Valid types are 'c 'p 'q 'b 'r 'g 'y and 'basket.
+ (define (token-type token)
+   (second token))
+
+ ;; Returns the token's direction
+ ;; Valid types are f0 to f3
+ (define (token-dir token)
+   (last token))
+
+ ;; The basket is the final token, when reached the tape is accepted.
+ (define (basket? token)
+   (eq? (token-type token) 'basket))
+
+ (define (writer? token)
+   (if (member (token-type token) '(b r g y)) true false))
+
+ (define (carrier? token)
+   (eq? (token-type token) 'c))
+
+ (define (blue-red-branch? token)
+   (eq? (token-type token) 'p))
+
+ (define (green-yellow-branch? token)
+   (eq? (token-type token) 'q))
+
+ (define (branch? token)
+   (or (blue-red-branch? token)
+       (green-yellow-branch? token)))
+
+ ;; Translates the f0..f4 into more verbose symbols
+ (define (one-way-dir dir)
+   (cond [(eq? dir 'f0) 'right->left]
+         [(eq? dir 'f1) 'bottom->top]
+         [(eq? dir 'f2) 'left->right]
+         [(eq? dir 'f3) 'top->bottom]
+         [else null]))
+
+ ;; Returns the direction to take based on the token's orientation and the
+ ;; first color in tape
+ (define (next-branch-dir tape token)
+   (cadr (assoc (if (null? tape) 
+                    null 
+                    (car tape))
+                (cadr (assoc (token-dir token)
+                             (cadr (assoc (token-type token) *branch-dirs*)))))))
+
+ ;; These definitions go into the machine's code
+ (define (helper-defs)
+   '((define (blue? color)
+       (eq? color 'b))
+     (define (red? color)
+       (eq? color 'r))
+     (define (green? color)
+       (eq? color 'g))
+     (define (yellow? color)
+       (eq? color 'y))
+     (define (next tape)
+       (if (null? tape) null (car tape)))
+     (define (tail tape)
+       (if (null? tape) null (cdr tape)))
+     (define (accept tape)
+       (list 'accepted tape))
+     (define (reject tape)
+       (list 'rejected tape))
+     ))
+
+ ;; Returns the code for a branch (tokens 'p and 'q)
+ (define (branch-code token tokens)
+   (define (branch color tape)
+     (let ([tok (get-token (next-pos (token-pos token)
+                                     (next-branch-dir (list color) token))
+                           tokens)])
+       (if (null? tok) 
+           '(reject tape-tail) 
+           (if (basket? tok)
+               `(accept ,tape) 
+               `(,(token-id (if (carrier? tok) 
+                                (advance tok tokens) 
+                                tok))
+                 ,tape)))))
+
+   `(define (,(token-id token) tape)
+      (let ([color (next tape)]
+            [tape-tail (tail tape)])
+        ,(cond [(blue-red-branch? token)
+                `(cond [(blue? color) ,(branch 'b 'tape-tail)]
+                       [(red? color) ,(branch 'r 'tape-tail)]
+                       [else ,(branch null 'tape)])]  ; maybe green or yellow
+               [(green-yellow-branch? token)
+                `(cond [(green? color) ,(branch 'g 'tape-tail)]
+                       [(yellow? color) ,(branch 'y 'tape-tail)]
+                       [else ,(branch null 'tape)])] ; maybe red or green
+               [else raise("Invalid token color")]))))
+
+ ;; Returns the code for the writers (tokens 'b 'r 'g and 'y)
+ (define (writer-code token tokens)
+   `(define (,(token-id token) tape)
+      ,(let ([tok (get-token (next-pos (token-pos token) (one-way-dir (token-dir token)))
+                             tokens)])
+         (if (null? tok) 
+             '(reject tape) 
+             (if (basket? tok)
+                 '(accept tape) 
+                 `(,(token-id (if (carrier? tok) 
+                                  (advance tok tokens) 
+                                  tok))
+                   (append tape (list ',(token-type token)))))))))
+
+ ;; Returns the token at position pos
+ (define (get-token pos tokens)
+   (if (null? tokens)
+       null
+       (if (equal? (token-pos (car tokens)) pos)
+           (car tokens)
+           (get-token pos (cdr tokens)))))
+
+ ;; Returns the next position in direction dir from position pos
+ (define (next-pos pos dir)
+   (let ([col (first pos)]
+         [row (second pos)])
+     (cond [(or (eq? dir 'right->left)
+                (eq? dir 'left))
+            (list (- col 1) row)]
+           [(or (eq? dir 'bottom->top)
+                (eq? dir 'up)) 
+            (list col (- row 1))]
+           [(or (eq? dir 'left->right) 
+                (eq? dir 'right)) 
+            (list (+ col 1) row)]
+           [(or (eq? dir 'top->bottom) 
+                (eq? dir 'down)) 
+            (list col (+ row 1))]
+           [else null])))
+
+ ;; Returns the first non carrier token, when starting at token one-way-token.
+ ;; one-way-token should be of type carrier or writer.
+ (define (advance one-way-token tokens)
+   (define (next-token one-way-token)
+     (if (null? one-way-token)
+         null
+         (get-token (next-pos (token-pos one-way-token) 
+                              (one-way-dir (token-dir one-way-token)))
+                    tokens)))
+
+   (let ([next (next-token one-way-token)])
+     (if (null? next)
+         null
+         (if (carrier? next)
+             (advance next tokens)
+             next))))
+
+ ;; Returns a list with the machine's code
+ (define (build-machine hardware-desc)
+
+   (define (branch-defs tokens)
+     (map (lambda (tok) (branch-code tok tokens)) (filter branch? tokens)))
+
+   (define (writer-defs tokens)
+     (map (lambda (tok) (writer-code tok tokens)) (filter writer? tokens)))
+
+   (let* ([level (get-level hardware-desc)]
+          [spos (get-start-pos level)]
+          [tokens (split-into-tokens hardware-desc)]
+          [token (get-token spos tokens)])
+     (append '(lambda (tape))
+             (helper-defs)
+             (branch-defs tokens)
+             (writer-defs tokens)
+             `((define (,(token-id (basket-tok level)) tape)
+                 (accept tape)))
+             `((,(token-id (if (carrier? token) 
+                               (advance token tokens) 
+                               token))
+                tape)))))
+
+ (define (run machine tape)
+   ;; see http://docs.racket-lang.org/guide/eval.html#(part._namespaces)
+   ;; for help on make-base-namespace
+   ((eval machine) tape (make-base-namespace)))
+
+ )
+
+;; sample usage:
+
+(define machine (build-machine "?lvl=25&code=c12:5f3;c12:9f3;p12:6f3;g11:6f1;c11:5f2;y13:6f1;c13:5f0;c12:7f3;q12:8f3;c13:7f0;c11:7f2;r13:8f1;b11:8f1;"))
+
+;; run it!
+(run machine '(b r b))
+
+;; show machine's code
+machine
